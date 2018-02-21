0de61dd6382c6a79feaf8db45d9753947df6f0ab
Paul B <paul1994@hotmail.com>
2018-02-20 20:42:59 -0800

    modified files

--- asg_lb_ec2/main.tf
+++ asg_lb_ec2/main.tf
@@ -5,39 +5,68 @@ variable "server_port" {
   description = "the port that the webserver will listen on"
   default = 8080
 }
+data "aws_availability_zones" "all" {}
+# data "aws_availability_zones" "available" {}
+resource "aws_security_group" "instance" {
+  name        = "terraform-example-instance"
+  ingress {
+    from_port     = "${var.server_port}"
+    to_port       = "${var.server_port}"
+    protocol      = "tcp"
+    cidr_blocks   = ["0.0.0.0/0"]
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 # resource "aws_elb" "eample" {
 #   name          = "terraform-asg-example"
 # }
-# resource "aws_launch_configuration" "example" {
-#   image_id      = "ami-79873901"
-#   instance_type = "t2.micro"
-#   security_groups = ["sg-359f744a","sg-18df3467"]
-#   user_data = <<-EOF
-#               #!/bin/bash
-#               echo "Hello, World This is a Test" > index.html
-#               nohup busybox httpd - f -p ${var.server_port} &
-#               EOF
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
-resource "aws_instance" "example" {
-  count         = 2
-  ami           = "ami-79873901"
-  instance_type = "t2.micro"
-  vpc_security_group_ids = ["sg-359f744a","sg-18df3467"]
-  subnet_id     = "subnet-26e5e56e"
+resource "aws_launch_configuration" "example" {
+  image_id      = "ami-79873901"
+  instance_type = "t1.micro"
+  security_groups = ["${aws_security_group.instance.id}"]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World This is a Test" > index.html
               nohup busybox httpd - f -p ${var.server_port} &
               EOF
-tags {
-  Name = "Terraform-example"
-  Application = "test"
-  OS          = "Ubuntu 16 svr"
- }
+  lifecycle {
+    create_before_destroy = true
+  }
 }
-output "public_ip" {
-  value = "${aws_instance.example.*.public_ip}"
+resource "aws_autoscaling_group" "example" {
+  launch_configuration  = "${aws_launch_configuration.example.id}"
+  availability_zones    = ["${data.aws_availability_zones.all.names}"]
+  min_size      = 2
+  max_size      = 3
+
+  tag {
+    key = " Name"
+    value = "Terraform-ASG-example"
+    key = "Application"
+    value = "test"
+    propagate_at_launch = true
+  }
 }
+# resource "aws_instance" "example" {
+#   count         = 2
+#   ami           = "ami-79873901"
+#   instance_type = "t2.micro"
+#   vpc_security_group_ids = ["sg-359f744a","sg-18df3467"]
+#   subnet_id     = "subnet-26e5e56e"
+#   user_data = <<-EOF
+#               #!/bin/bash
+#               echo "Hello, World This is a Test" > index.html
+#               nohup busybox httpd - f -p ${var.server_port} &
+#               EOF
+# tags {
+#   Name = "Terraform-example"
+#   Application = "test"
+#   OS          = "Ubuntu 16 svr"
+#  }
+# }
+# output "public_ip" {
+#   value = "${aws_instance.example.*.public_ip}"
+# }
